0a56dd726b6a196a6c252a606307f233cefd92fa
MarcosDMT <marcosgav80@gmail.com>
2023-03-08 00:06:41 +0300

    polished the experience stepper

--- components/experience.js
+++ components/experience.js
@@ -23,52 +23,114 @@ const Experience = () => {
 
       <div className="flex gap-8">
         <div className=" my-8 w-4/12">
-          <ul className="space-y-4">
+          <ul className="flex flex-col gap-6 border-l-2 border-tertiary px-2">
             {steps.map((step, index) => (
-                    <li className={`${activeStep ? 'text-secondary text-sm cursor-pointer': 'text-gray cursor-pointer'}`} key={index} onClick={()=> setActiveStep(index)}>
-                     {step}
-                   </li>
-             ))}
+              <li
+                className={`${
+                  index === activeStep
+                    ? "text-secondary text-sm cursor-pointer bg-tertiary py-3 px-3 text-sm uppercase"
+                    : "text-gray cursor-pointer py-3 px-3 text-sm uppercase"
+                }`}
+                key={index}
+                onClick={() => setActiveStep(index)}
+              >
+                {step}
+              </li>
+            ))}
           </ul>
         </div>
         <div className="my-8 w-9/12">
-        {activeStep === 0 && <>
-        <div className="space-y-4">
-        <h2 className="text-white text-xl">Engineer <span className="text-secondary"> @ Dynamic Mobility</span></h2>
-          <p className="text-white text-sm">May 2018 - Present</p>
-          <ul className="marker:text-secondary text-gray text-sm list-disc space-y-4 px-4">
-            <li>Write modern, performant, maintainable code for a diverse array of client and internal projects</li>
-            <li>Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Gatsby, React, Craft, WordPress, Prismic, and Netlify</li>
-            <li>Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis</li>
-          </ul>
-        </div>
-          
-          </>}
+          {activeStep === 0 && (
+            <>
+              <div className="space-y-4">
+                <h2 className="text-white">
+                  Front End Developer{" "}
+                  <span className="text-secondary font-semibold text-sm">
+                    {" "}
+                    @ Dynamic Mobility Technology
+                  </span>
+                </h2>
+                <p className="text-gray text-sm">Sep 2022 - Present</p>
+                <ul className="marker:text-secondary text-gray text-sm list-disc space-y-4 px-4">
+                  <li>
+                    Write modern, performant, maintainable code for a diverse
+                    array of client and internal projects
+                  </li>
+                  <li>
+                    Work with a variety of different languages, platforms,
+                    frameworks, and content management systems such as
+                    JavaScript, TypeScript, Gatsby, React, Craft, WordPress,
+                    Prismic, and Netlify
+                  </li>
+                  <li>
+                    Communicate with multi-disciplinary teams of engineers,
+                    designers, producers, and clients on a daily basis
+                  </li>
+                </ul>
+              </div>
+            </>
+          )}
 
-          {activeStep === 1 && <>
-        <div className="space-y-4">
-        <h2 className="text-white text-xl">Engineer <span className="text-secondary"> @ Optica</span></h2>
-          <p className="text-white text-sm">May 2018 - Present</p>
-          <ul className="marker:text-secondary text-gray text-sm list-disc space-y-4 px-4">
-            <li>Write modern, performant, maintainable code for a diverse array of client and internal projects</li>
-            <li>Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Gatsby, React, Craft, WordPress, Prismic, and Netlify</li>
-            <li>Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis</li>
-          </ul>
-        </div>
-          
-          </>}
-          {activeStep === 2 && <>
-        <div className="space-y-4">
-        <h2 className="text-white text-xl">Engineer <span className="text-secondary"> @ Coretec Solutions Africa</span></h2>
-          <p className="text-white text-sm">May 2018 - Present</p>
-          <ul className="marker:text-secondary text-gray text-sm list-disc space-y-4 px-4">
-            <li>Write modern, performant, maintainable code for a diverse array of client and internal projects</li>
-            <li>Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Gatsby, React, Craft, WordPress, Prismic, and Netlify</li>
-            <li>Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis</li>
-          </ul>
-        </div>
-          
-          </>}
+          {activeStep === 1 && (
+            <>
+              <div className="space-y-4">
+                <h2 className="text-white">
+                  Backend Developer{" "}
+                  <span className="text-secondary font-semibold">
+                    {" "}
+                    @ Optica
+                  </span>
+                </h2>
+                <p className="text-white text-sm">May 2018 - Present</p>
+                <ul className="marker:text-secondary text-gray text-sm list-disc space-y-4 px-4">
+                  <li>
+                    Write modern, performant, maintainable code for a diverse
+                    array of client and internal projects
+                  </li>
+                  <li>
+                    Work with a variety of different languages, platforms,
+                    frameworks, and content management systems such as
+                    JavaScript, TypeScript, Gatsby, React, Craft, WordPress,
+                    Prismic, and Netlify
+                  </li>
+                  <li>
+                    Communicate with multi-disciplinary teams of engineers,
+                    designers, producers, and clients on a daily basis
+                  </li>
+                </ul>
+              </div>
+            </>
+          )}
+          {activeStep === 2 && (
+            <>
+              <div className="space-y-4">
+                <h2 className="text-white">
+                  Systems Developer{" "}
+                  <span className="text-secondary font-semibold">
+                    {" "}
+                    @ Coretec Solutions Africa
+                  </span>
+                </h2>
+                <p className="text-white text-sm">May 2018 - Present</p>
+                <ul className="marker:text-secondary text-gray text-sm list-disc space-y-4 px-4">
+                  <li>
+                    Write modern, performant, maintainable code for a diverse
+                    array of client and internal projects
+                  </li>
+                  <li>
+                    Work with a variety of different languages, platforms,
+                    frameworks, and content management systems such as
+                    JavaScript, TypeScript, Gatsby, React, Craft, WordPress,
+                    Prismic, and Netlify
+                  </li>
+                  <li>
+                    Communicate with multi-disciplinary teams of engineers,
+                    designers, producers, and clients on a daily basis
+                  </li>
+                </ul>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </section>
--- components/projects.js
+++ components/projects.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { BiLinkExternal } from "react-icons/bi";
+import { AiOutlineGithub } from "react-icons/ai";
 
 const Projects = () => {
   return (
     <>
-      <section className="px-36 h-screen py-12">
+      <section className="px-36 py-12">
         <div className="flex gap-8 items-center w-6/12 px-12">
           <span className="text-secondary text-sm font-semibold">03.</span>
           <h1 className="text-white font-black text-2xl">
@@ -11,53 +13,118 @@ const Projects = () => {
           </h1>
           <div class="flex-grow h-px bg-gray"></div>
         </div>
-        <section className="flex py-12 px-12">
-          <div className="project_img relative cursor-pointer"></div>
-          <div className="flex flex-col px-12">
-            <p className="text-secondary">Featured Project</p>
-            <h1 className="text-white font-black text-3xl">Bloow</h1>
-          </div>
 
-          <div className="absolute bg-tertiary z-40 h-36 w-1/2 right-24 rounded my-20 px-8 py-4 text-gray">
-            A web app for visualizing personalized Spotify data. View your top
-            artists, top tracks, recently played tracks, and detailed audio
-            information about each track. Create and save new playlists of
-            recommended tracks based on your existing playlists and more.
-          </div>
+
+        <section className="flex items-center py-12 px-12 gap-4 h-screen">
+          <aside className="project_img relative cursor-pointer h-96 rounded w-1/2"></aside>
+          <aside className="w-1/2 flex flex-col gap-4">
+            <div className="flex flex-col">
+              <p className="text-secondary text-right">Featured Project</p>
+              <h1 className="text-white font-black text-3xl text-right">
+                Bloow
+              </h1>
+            </div>
+            <div className="bg-tertiary z-40 h-32 w-full rounded px-8 py-4 text-gray">
+              <p className="text-sm">
+                A web app for visualizing personalized Spotify data. View your
+                top artists, top tracks, recently played tracks, and detailed
+                audio information about each track. Create and save new
+                playlists of recommended tracks based on your existing playlists
+                and more.
+              </p>
+            </div>
+            <div className="flex items-center justify-end gap-4 text-gray text-sm">
+              <p>Next.js</p>
+              <p>Next.js</p>
+              <p>Next.js</p>
+            </div>
+            <div className="flex items-center justify-end gap-4 text-white">
+              <AiOutlineGithub
+                title="Github"
+                className="text-2xl cursor-pointer"
+              />
+              <BiLinkExternal
+                title="External Link"
+                className="text-2xl cursor-pointer"
+              />
+            </div>
+          </aside>
         </section>
-      </section>
 
-      <section className="px-36 h-screen py-12">
-        <section className="flex justify-end py-12 px-12">
-          <div className="flex flex-col px-12">
-            <p className="text-secondary">Featured Project</p>
-            <h1 className="text-white font-black text-3xl">Bloow</h1>
-          </div>
-          <div className="absolute bg-tertiary z-40 h-36 w-1/2 left-24 rounded my-20 px-8 py-4 text-gray">
-            A web app for visualizing personalized Spotify data. View your top
-            artists, top tracks, recently played tracks, and detailed audio
-            information about each track. Create and save new playlists of
-            recommended tracks based on your existing playlists and more.
-          </div>
-          <div className="project_img relative"></div>
+        <section className="flex items-center py-12 px-12 gap-4 h-screen">
+          <aside className="w-1/2 flex flex-col gap-4">
+            <div className="flex flex-col">
+              <p className="text-secondary text-left">Featured Project</p>
+              <h1 className="text-white font-black text-3xl text-left">
+                Bloow
+              </h1>
+            </div>
+            <div className="bg-tertiary z-40 h-32 w-full rounded px-8 py-4 text-gray">
+              <p className="text-sm">
+                A web app for visualizing personalized Spotify data. View your
+                top artists, top tracks, recently played tracks, and detailed
+                audio information about each track. Create and save new
+                playlists of recommended tracks based on your existing playlists
+                and more.
+              </p>
+            </div>
+            <div className="flex items-center justify-start gap-4 text-gray text-sm">
+              <p>Next.js</p>
+              <p>Next.js</p>
+              <p>Next.js</p>
+            </div>
+            <div className="flex items-center justify-start gap-4 text-white">
+              <AiOutlineGithub
+                title="Github"
+                className="text-2xl cursor-pointer"
+              />
+              <BiLinkExternal
+                title="External Link"
+                className="text-2xl cursor-pointer"
+              />
+            </div>
+          </aside>
+          <aside className="project_img relative cursor-pointer h-96 rounded w-1/2"></aside>
         </section>
-      </section>
 
-      <section className="px-36 h-screen py-12">
-        <section className="flex py-12 px-12">
-          <div className="project_img relative"></div>
-          <div className="flex flex-col px-12">
-            <p className="text-secondary">Featured Project</p>
-            <h1 className="text-white font-black text-3xl">Bloow</h1>
-          </div>
 
-          <div className="absolute bg-tertiary z-40 h-36 w-1/2 right-24 rounded my-20 px-8 py-4 text-gray">
-            A web app for visualizing personalized Spotify data. View your top
-            artists, top tracks, recently played tracks, and detailed audio
-            information about each track. Create and save new playlists of
-            recommended tracks based on your existing playlists and more.
-          </div>
+        <section className="flex items-center py-12 px-12 gap-4 h-screen">
+          <aside className="project_img relative cursor-pointer h-96 rounded w-1/2"></aside>
+          <aside className="w-1/2 flex flex-col gap-4">
+            <div className="flex flex-col">
+              <p className="text-secondary text-right">Featured Project</p>
+              <h1 className="text-white font-black text-3xl text-right">
+                Bloow
+              </h1>
+            </div>
+            <div className="bg-tertiary z-40 h-32 w-full rounded px-8 py-4 text-gray">
+              <p className="text-sm">
+                A web app for visualizing personalized Spotify data. View your
+                top artists, top tracks, recently played tracks, and detailed
+                audio information about each track. Create and save new
+                playlists of recommended tracks based on your existing playlists
+                and more.
+              </p>
+            </div>
+            <div className="flex items-center justify-end gap-4 text-gray text-sm">
+              <p>Next.js</p>
+              <p>Next.js</p>
+              <p>Next.js</p>
+            </div>
+            <div className="flex items-center justify-end gap-4 text-white">
+              <AiOutlineGithub
+                title="Github"
+                className="text-2xl cursor-pointer"
+              />
+              <BiLinkExternal
+                title="External Link"
+                className="text-2xl cursor-pointer"
+              />
+            </div>
+          </aside>
         </section>
+
+
       </section>
     </>
   );
