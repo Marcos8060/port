6901e68bd72a69fa04dc116387d3f18fb2f32301
MarcosDMT <marcosgav80@gmail.com>
2023-03-09 01:08:55 +0300

    added responsiveness projects section

--- components/projects.js
+++ components/projects.js
@@ -14,6 +14,41 @@ const Projects = () => {
           <div class="md:block hidden flex-grow h-px bg-gray"></div>
         </div>
 
+        <section className="md:flex items-center py-12 sm:px-12 gap-4 md:h-screen h-auto space-y-4">
+          <aside className="project_img relative cursor-pointer md:h-96 h-72 rounded md:w-1/2"></aside>
+          <aside className="md:w-1/2 w-full flex flex-col gap-4">
+            <div className="flex flex-col">
+              <p className="text-secondary text-right">Featured Project</p>
+              <h1 className="text-white font-black text-3xl text-right">
+                Bloow
+              </h1>
+            </div>
+            <div className="bg-tertiary z-40 md:h-32 h-auto w-full rounded px-8 py-4 text-gray">
+              <p className="text-sm">
+                A web app for visualizing personalized Spotify data. View your
+                top artists, top tracks, recently played tracks, and detailed
+                audio information about each track. Create and save new
+                playlists of recommended tracks based on your existing playlists
+                and more.
+              </p>
+            </div>
+            <div className="flex items-center justify-end gap-4 text-gray text-sm">
+              <p>Next.js</p>
+              <p>Next.js</p>
+              <p>Next.js</p>
+            </div>
+            <div className="flex items-center justify-end gap-4 text-white">
+              <AiOutlineGithub
+                title="Github"
+                className="text-2xl cursor-pointer"
+              />
+              <BiLinkExternal
+                title="External Link"
+                className="text-2xl cursor-pointer"
+              />
+            </div>
+          </aside>
+        </section>
 
         <section className="md:flex items-center py-12 sm:px-12 gap-4 md:h-screen h-auto space-y-4">
           <aside className="project_img relative cursor-pointer md:h-96 h-72 rounded md:w-1/2"></aside>
@@ -51,53 +86,16 @@ const Projects = () => {
           </aside>
         </section>
 
-        <section className="flex items-center py-12 px-12 gap-4 h-screen">
-          <aside className="w-1/2 flex flex-col gap-4">
-            <div className="flex flex-col">
-              <p className="text-secondary text-left">Featured Project</p>
-              <h1 className="text-white font-black text-3xl text-left">
-                Bloow
-              </h1>
-            </div>
-            <div className="bg-tertiary z-40 h-32 w-full rounded px-8 py-4 text-gray">
-              <p className="text-sm">
-                A web app for visualizing personalized Spotify data. View your
-                top artists, top tracks, recently played tracks, and detailed
-                audio information about each track. Create and save new
-                playlists of recommended tracks based on your existing playlists
-                and more.
-              </p>
-            </div>
-            <div className="flex items-center justify-start gap-4 text-gray text-sm">
-              <p>Next.js</p>
-              <p>Next.js</p>
-              <p>Next.js</p>
-            </div>
-            <div className="flex items-center justify-start gap-4 text-white">
-              <AiOutlineGithub
-                title="Github"
-                className="text-2xl cursor-pointer"
-              />
-              <BiLinkExternal
-                title="External Link"
-                className="text-2xl cursor-pointer"
-              />
-            </div>
-          </aside>
-          <aside className="project_img relative cursor-pointer h-96 rounded w-1/2"></aside>
-        </section>
-
-
-        <section className="flex items-center py-12 px-12 gap-4 h-screen">
-          <aside className="project_img relative cursor-pointer h-96 rounded w-1/2"></aside>
-          <aside className="w-1/2 flex flex-col gap-4">
+        <section className="md:flex items-center py-12 sm:px-12 gap-4 md:h-screen h-auto space-y-4">
+          <aside className="project_img relative cursor-pointer md:h-96 h-72 rounded md:w-1/2"></aside>
+          <aside className="md:w-1/2 w-full flex flex-col gap-4">
             <div className="flex flex-col">
               <p className="text-secondary text-right">Featured Project</p>
               <h1 className="text-white font-black text-3xl text-right">
                 Bloow
               </h1>
             </div>
-            <div className="bg-tertiary z-40 h-32 w-full rounded px-8 py-4 text-gray">
+            <div className="bg-tertiary z-40 md:h-32 h-auto w-full rounded px-8 py-4 text-gray">
               <p className="text-sm">
                 A web app for visualizing personalized Spotify data. View your
                 top artists, top tracks, recently played tracks, and detailed
