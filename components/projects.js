791739b675122ffe78a83ccf1ffc7595c6cb3f61
MarcosDMT <marcosgav80@gmail.com>
2023-03-09 01:06:19 +0300

    added responsiveness for about page

--- components/projects.js
+++ components/projects.js
@@ -5,26 +5,26 @@ import { AiOutlineGithub } from "react-icons/ai";
 const Projects = () => {
   return (
     <>
-      <section className="px-36 py-12">
-        <div className="flex gap-8 items-center w-6/12 px-12">
+      <section className="sm:px-36 px-4 py-12">
+        <div className="flex gap-8 items-center md:w-6/12 mx-auto w-full sm:px-12">
           <span className="text-secondary text-sm font-semibold">03.</span>
           <h1 className="text-white font-black text-2xl">
             Some Things I've Built
           </h1>
-          <div class="flex-grow h-px bg-gray"></div>
+          <div class="md:block hidden flex-grow h-px bg-gray"></div>
         </div>
 
 
-        <section className="flex items-center py-12 px-12 gap-4 h-screen">
-          <aside className="project_img relative cursor-pointer h-96 rounded w-1/2"></aside>
-          <aside className="w-1/2 flex flex-col gap-4">
+        <section className="md:flex items-center py-12 sm:px-12 gap-4 md:h-screen h-auto space-y-4">
+          <aside className="project_img relative cursor-pointer md:h-96 h-72 rounded md:w-1/2"></aside>
+          <aside className="md:w-1/2 w-full flex flex-col gap-4">
             <div className="flex flex-col">
               <p className="text-secondary text-right">Featured Project</p>
               <h1 className="text-white font-black text-3xl text-right">
                 Bloow
               </h1>
             </div>
-            <div className="bg-tertiary z-40 h-32 w-full rounded px-8 py-4 text-gray">
+            <div className="bg-tertiary z-40 md:h-32 h-auto w-full rounded px-8 py-4 text-gray">
               <p className="text-sm">
                 A web app for visualizing personalized Spotify data. View your
                 top artists, top tracks, recently played tracks, and detailed
