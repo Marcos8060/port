3baba323b2814b83bf0d28a20e136e77f37bb2bf
MarcosDMT <marcosgav80@gmail.com>
2023-03-10 07:40:29 +0300

    polished the about page

--- components/fab.js
+++ components/fab.js
@@ -46,7 +46,7 @@ const Fab = () => {
         title="Follow me on Twitter"
         class="fixed z-40 bottom-32 right-8 bg-blue w-12 h-12 p-2 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-blue-700 hover:drop-shadow-2xl duration-300"
       >
-        <div className="text-xs rotate-90 flex space-x-1">
+        <div className="md:block hidden text-xs rotate-90 flex space-x-1">
             <p>m</p>
             <p>a</p>
             <p>r</p>
