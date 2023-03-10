03d8d96072d87f6985f2e4c880fb269bc2383611
MarcosDMT <marcosgav80@gmail.com>
2023-03-10 07:42:18 +0300

    polished the about page

--- components/fab.js
+++ components/fab.js
@@ -8,7 +8,7 @@ const Fab = () => {
     <>
       <button
         title="My Github Activity"
-        class="fixed z-40 bottom-32 left-8 bg-blue w-12 h-12 p-2 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-blue-700 hover:drop-shadow-2xl hover:animate-bounce duration-300"
+        className="md:block hidden fixed z-40 bottom-32 left-8 bg-blue w-12 h-12 p-2 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-blue-700 hover:drop-shadow-2xl hover:animate-bounce duration-300"
       >
         <a
           className="text-white"
@@ -20,7 +20,7 @@ const Fab = () => {
       </button>
       <button
         title="Follow me on LinkedIn"
-        class="fixed z-40 bottom-20 left-8 bg-blue w-12 h-12 p-2 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-blue-700 hover:drop-shadow-2xl hover:animate-bounce duration-300"
+        className="md:block hidden fixed z-40 bottom-20 left-8 bg-blue w-12 h-12 p-2 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-blue-700 hover:drop-shadow-2xl hover:animate-bounce duration-300"
       >
         <a
           className="text-white"
@@ -32,7 +32,7 @@ const Fab = () => {
       </button>
       <button
         title="Follow me on Twitter"
-        class="fixed z-40 bottom-8 left-8 bg-blue w-12 h-12 p-2 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-blue-700 hover:drop-shadow-2xl hover:animate-bounce duration-300"
+        className="md:block hidden fixed z-40 bottom-8 left-8 bg-blue w-12 h-12 p-2 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-blue-700 hover:drop-shadow-2xl hover:animate-bounce duration-300"
       >
         <a
           className="text-white"
@@ -44,7 +44,7 @@ const Fab = () => {
       </button>
       <button
         title="Follow me on Twitter"
-        class="fixed z-40 bottom-32 right-8 bg-blue w-12 h-12 p-2 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-blue-700 hover:drop-shadow-2xl duration-300"
+        className="md:block hidden fixed z-40 bottom-32 right-8 bg-blue w-12 h-12 p-2 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-blue-700 hover:drop-shadow-2xl duration-300"
       >
         <div className="md:block hidden text-xs rotate-90 flex space-x-1">
             <p>m</p>
