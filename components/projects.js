146b220fd919ce757962d2495b54c2e04dc79757
MarcosDMT <marcosgav80@gmail.com>
2023-07-24 10:09:40 +0300

    made a minor change

--- components/projects.js
+++ components/projects.js
@@ -39,7 +39,7 @@ const Projects = () => {
               <p>.NET</p>
             </div>
             <div className="flex items-center justify-start gap-4 text-white">
-              <Link
+              {/* <Link
                 target="_blank"
                 href="https://github.com/mutuajoseph/AICE-WEBSITE"
               >
@@ -47,9 +47,9 @@ const Projects = () => {
                   title="Github"
                   className="text-2xl cursor-pointer"
                 />
-              </Link>
+              </Link> */}
 
-              <Link target="_blank" href="https://www.aiceafrica.com/">
+              <Link target="_blank" href="https://app.risksolve.io/login">
                 <BiLinkExternal
                   title="External Link"
                   className="text-2xl cursor-pointer"
